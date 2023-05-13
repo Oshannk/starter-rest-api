e5c3bb56e04004c9f3c9f2c9523ef67934d3002f
oshannk <oshan.n@hatchyard.io>
2023-05-13 12:09:31 +0530

    /participants

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Create or Update an item
-app.post('/participants/:key', async (req, res) => {
+app.post('/participants/add', async (req, res) => {
   console.log(req.body)
 
   // const col = req.params.col
   const key = req.params.key
+  const email = req.params.personal.email
   // console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection('participants').set(key, req.body)
+  const item = await db.collection('participants').set(email, req.body)
   console.log(JSON.stringify(item, null, 2))
   res.json(item).end()
 })
@@ -55,7 +56,7 @@ app.get('/participants/details/:email', async (req, res) => {
   // const col = req.params.col
   const email = req.params.email
   const item = (await db.collection('participants').get(email))?.props
-  const data = {...item.personal, ...item.active}
+  const data = {...item.personal, active: item.active}
   console.log(JSON.stringify(data, null, 2))
   res.json(data).end()
 })
