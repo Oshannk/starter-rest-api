cb0baf0ac256f2ae8c3cc3c6cd292f74186359d4
oshannk <oshan.n@hatchyard.io>
2023-05-13 19:24:13 +0530

    date

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const validateEmail = (email) => {
 function validateDate(dateString) {
   const regex = /^(\d{4})\/(\d{2})\/(\d{2})$/
   const match = dateString.match(regex)
+  console.log('dateString', dateString);
+  console.log('match', match);
   if (match === null) {
     return false
   }
