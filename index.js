524b01bebba4cd48dcd376dea2b4c0346695170c
oshannk <oshan.n@hatchyard.io>
2023-05-13 12:34:19 +0530

    Get a single item work

--- index.js
+++ index.js
@@ -18,15 +18,6 @@ app.post('/participants/add', async (req, res) => {
   res.json(item).end()
 })
 
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
 
 // Get a full listing
 app.get('/participants', async (req, res) => { 
@@ -53,7 +44,6 @@ app.get('/participants/details/deleted', async (req, res) => {
 
 // Get a single item
 app.get('/participants/details/:email', async (req, res) => {
-  // const col = req.params.col
   const email = req.params.email
   const item = (await db.collection('participants').get(email))?.props
   const data = {...item.personal, active: item.active}
@@ -61,7 +51,25 @@ app.get('/participants/details/:email', async (req, res) => {
   res.json(data).end()
 })
 
+// Get a single item work
+app.get('/participants/work/:email', async (req, res) => {
+  const email = req.params.email
+  const item = (await db.collection('participants').get(email))?.props.work
+  // const data = {...item.personal, active: item.active}
+  console.log(JSON.stringify(item, null, 2))
+  res.json(item).end()
+})
 
+
+// Delete an item
+app.delete('/:col/:key', async (req, res) => {
+  const col = req.params.col
+  const key = req.params.key
+  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+  const item = await db.collection(col).delete(key)
+  console.log(JSON.stringify(item, null, 2))
+  res.json(item).end()
+})
 
 
 
